ff1ded8267e6cbcd560aa40182f533ca90d94ea3
guudewie <philippthiessen12@gmail.com>
2023-09-01 16:23:17 +0200

    set up file structure

---
+++ main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("\nasync function weatherCalll(location) {\n    const url = `https://api.weatherapi.com/v1/forecast.json?key=e069f2b106c04ce29a1142633233108&q=${location}`;\n\n    try {\n        const response = await fetch(url);\n        const data = await response.json()\n\n        console.log(data)\n    } catch (error) {\n        alert(error)\n    } \n}\n\nweatherCalll(\"qjkhbckjb\")\n\n//# sourceURL=webpack://weather_app/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;

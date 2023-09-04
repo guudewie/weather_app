e96238484634fa3783a1e42247b4a3be2a78b0ba
guudewie <philippthiessen12@gmail.com>
2023-09-04 13:59:24 +0200

    finish ui styling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import weather from "./modules/api"
+import weather from "./modules/api";
 
 const form = document.querySelector("form");
 const input = document.querySelector("input.location");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    weather.getData(input.value)
-})
+  e.preventDefault();
+  weather.getData(input.value);
+});
--- src/modules/api.js
+++ src/modules/api.js
@@ -1,21 +1,17 @@
-
 const weather = (() => {
+  async function getData(location) {
+    const url = `https://api.weatherapi.com/v1/forecast.json?key=e069f2b106c04ce29a1142633233108&q=${location}`;
 
-    async function getData(location) {
-        const url = `https://api.weatherapi.com/v1/forecast.json?key=e069f2b106c04ce29a1142633233108&q=${location}`;
+    const response = await fetch(url);
+    const data = await response.json();
 
-        const response = await fetch(url);
-        const data = await response.json();
+    console.log(data);
+    return data;
+  }
 
-        console.log(data)
-        return data
-    }
-
-
-    return {
-        getData
-    }
-
+  return {
+    getData,
+  };
 })();
 
-export default weather
+export default weather;

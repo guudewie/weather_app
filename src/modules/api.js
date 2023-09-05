548be25750f59f5421a09600ed19a2d14f34bae9
guudewie <philippthiessen12@gmail.com>
2023-09-05 19:03:59 +0200

    remove ui error message after successfull api call

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,27 +1,29 @@
 const weather = (() => {
-  async function getData(location) {
-    const url = `https://api.weatherapi.com/v1/current.json?key=e069f2b106c04ce29a1142633233108&q=${location}`;
-    const errorDom = document.querySelector(".error")
+    async function getData(location) {
+        const url = `https://api.weatherapi.com/v1/current.json?key=e069f2b106c04ce29a1142633233108&q=${location}`;
+        const errorDom = document.querySelector(".error");
 
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
 
-      if (data.error) {
-        errorDom.textContent= data.error.message
-        return null
-      }
+            if (data.error) {
+                errorDom.textContent = data.error.message;
+                return null;
+            }
 
-      return data;
-    } catch (error) {
-      alert("There seems to be an issue. Let´s try it one more time.");
-      return null;
+            errorDom.textContent = "";
+
+            return data;
+        } catch (error) {
+            alert("There seems to be an issue. Let´s try it one more time.");
+            return null;
+        }
     }
-  }
 
-  return {
-    getData,
-  };
+    return {
+        getData,
+    };
 })();
 
 export default weather;
